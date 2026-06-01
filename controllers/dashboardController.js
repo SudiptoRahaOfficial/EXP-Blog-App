f43fd51092711028a9a367372c328cfa6dbc48cd
SudiptoRahaOfficial <sudiptoraha.official@gmail.com>
2026-06-01 13:43:03 +0600

    implemented createProfile & editProfile routes | conditionally rendering dashboard

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,13 +1,45 @@
+// importing Profile model
+const Profile = require('../models/Profile')
+
 // importing Flash class for alerts
 const Flash = require('../utils/Flash')
 
 // controller function for dashboard get route
-const dashboardGetController = (req, res, next) => {
-	res.render('pages/dashboard/dashboard', {
-		title: 'Dashboard | EXP BLOG',
-		flashMessage: Flash.getMessage(req),
-	})
+const dashboardGetController = async (req, res, next) => {
+	try {
+		// approve dashboard access if user's profile exists
+		const profile = await Profile.findOne({ user: req.user._id })
+		if (profile) {
+			return res.render('pages/dashboard/dashboard', {
+				title: 'Dashboard | EXP BLOG',
+				flashMessage: Flash.getMessage(req),
+			})
+		}
+
+		// redirecting to create-profile if profile not exists
+		return res.redirect('/dashboard/create-profile')
+	} catch (err) {
+		next(err)
+	}
 }
 
+// controller funciton for create-profile get route
+const createProfileGetController = (req, res, next) => {}
+
+// controller funciton for create-profile post route
+const createProfilePostController = (req, res, next) => {}
+
+// controller funciton for edit-profile get route
+const editProfileGetController = (req, res, next) => {}
+
+// controller funciton for edit-profile post route
+const editProfilePostController = (req, res, next) => {}
+
 // exporting controllers
-module.exports = { dashboardGetController }
+module.exports = {
+	dashboardGetController,
+	createProfileGetController,
+	createProfilePostController,
+	editProfileGetController,
+	editProfilePostController,
+}
--- routes/dashboardRoute.js
+++ routes/dashboardRoute.js
@@ -5,9 +5,27 @@ const router = require('express').Router()
 const { isAuthenticated } = require('../middlewares/authMiddleware')
 
 // importing controllers
-const { dashboardGetController } = require('../controllers/dashboardController')
+const {
+	dashboardGetController,
+	createProfileGetController,
+	createProfilePostController,
+	editProfileGetController,
+	editProfilePostController,
+} = require('../controllers/dashboardController')
 
-// all routes
+// ALL DASHBOARD ROUTES ----------
+
+// edit-profile get route
+router.get('/edit-profile', editProfileGetController)
+// edit-profile post route
+router.post('/edit-profile', editProfilePostController)
+
+// create-profile get route
+router.get('/create-profile', createProfileGetController)
+// create-profile post route
+router.post('/create-profile', createProfilePostController)
+
+// dashboard route
 router.get('/', isAuthenticated, dashboardGetController)
 
 // exporting router

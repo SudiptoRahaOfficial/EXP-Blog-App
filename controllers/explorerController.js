db6340f836d4a078e8da66af25bc6ead96e1aeda
SudiptoRahaOfficial <sudiptoraha.official@gmail.com>
2026-06-08 19:25:40 +0600

    added pagination system's backend functionality

--- controllers/explorerController.js
+++ controllers/explorerController.js
@@ -7,24 +7,37 @@ const generateFilterObj = require('../utils/generateFilter')
 
 // controller function for explorer get route
 const explorerGetController = async (req, res, next) => {
-	// extracting & setting filter
+	// setting properties at req.query
 	let filter = req.query.filter || 'latest'
+	let currentPage = parseInt(req.query.page) || 1
+
+	// initializing item-count per page
+	const itemPerPage = 10
 
 	// extracting filterObj & order
 	let { filterObj, order } = generateFilterObj(filter.toLowerCase())
 
 	try {
-		// fetching all stored posts from db
+		// fetching posts according to filter & pagination
 		let posts = await Post.find(filterObj)
 			.populate('author', 'username')
 			.sort(order === 1 ? '-createdAt' : 'createdAt')
+			.skip(itemPerPage * currentPage - itemPerPage)
+			.limit(itemPerPage)
+
+		// calculating total post and page count
+		let totalPost = await Post.countDocuments()
+		let totalPage = totalPost / itemPerPage
 
 		// rendering explorer page
 		res.render('pages/explorer/explorer', {
 			title: 'Explore All Posts | EXP BLOG',
 			flashMessage: Flash.getMessage(req),
-			filter,
 			posts,
+			filter,
+			itemPerPage,
+			currentPage,
+			totalPage,
 		})
 	} catch (err) {
 		next(err)
